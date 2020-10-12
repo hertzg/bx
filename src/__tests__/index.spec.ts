e3852e2b5f539da9f62344238c6437a49a9cd6e3
George Hertz <georgedot@gmail.com>
2020-10-12 03:34:09 +0200

    chore: format

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,23 +1,23 @@
-import bx from "../index";
+import bx from '../index';
 
 describe('bx', () => {
-    it('should produce 0 length buffer on empty string', () => {
-        expect(bx('')).toStrictEqual(Buffer.alloc(0));
-    });
-    it('should convert hex strings to buffer', () => {
-        expect(bx('feef')).toEqual(Buffer.from([0xfe, 0xef]));
-        expect(bx('feeffaaf')).toEqual(Buffer.from([0xfe, 0xef, 0xfa, 0xaf]));
-    });
-    it('should convert hex strings with separators to buffer', () => {
-        expect(bx('fe_ef af be b0 xba-ab')).toEqual(
-            Buffer.from([0xfe, 0xef, 0xaf, 0xbe, 0xb0, 0xba, 0xab])
-        );
-    });
-    it('should throw on invalid strings', () => {
-        expect(() => bx(' ')).toThrowError(TypeError);
-        expect(() => bx('zz')).toThrowError(TypeError);
-        expect(() => bx('0')).toThrowError(TypeError);
-        expect(() => bx('1')).toThrowError(TypeError);
-        expect(() => bx('deadbea')).toThrowError(TypeError);
-    });
+  it('should produce 0 length buffer on empty string', () => {
+    expect(bx('')).toStrictEqual(Buffer.alloc(0));
+  });
+  it('should convert hex strings to buffer', () => {
+    expect(bx('feef')).toEqual(Buffer.from([0xfe, 0xef]));
+    expect(bx('feeffaaf')).toEqual(Buffer.from([0xfe, 0xef, 0xfa, 0xaf]));
+  });
+  it('should convert hex strings with separators to buffer', () => {
+    expect(bx('fe_ef af be b0 xba-ab')).toEqual(
+      Buffer.from([0xfe, 0xef, 0xaf, 0xbe, 0xb0, 0xba, 0xab])
+    );
+  });
+  it('should throw on invalid strings', () => {
+    expect(() => bx(' ')).toThrowError(TypeError);
+    expect(() => bx('zz')).toThrowError(TypeError);
+    expect(() => bx('0')).toThrowError(TypeError);
+    expect(() => bx('1')).toThrowError(TypeError);
+    expect(() => bx('deadbea')).toThrowError(TypeError);
+  });
 });
