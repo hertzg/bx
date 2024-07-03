f9fd9c5800bd5717be4188b380e2d02eeb8bd336
Giorgi Kotchlamazashvili <georgedot@gmail.com>
2024-07-03 23:50:02 +0200

    chore: Update bx function comments and examples

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,26 @@
-  /**
-   * A method that accepts a string containing hex encoded buffer. 
-   * The string will be stripped of any non-hexadecimal symbols (eg: [^0-9-a-f]) and 
-   * if the resulting string has even length then it's used as new Uint8Array(value).buffer 
-   * or otherwise throws a TypeError.
-   *
-   * Examples of cases where it returns an ArrayBuffer
-   * ```ts
-   * const HEADER = bx('00 ff 00 ff'); // ArrayBuffer { [Uint8Contents]: <00 ff 00 ff>, byteLength: 4 }
-   * const SYNC1 = bx('00ff00ff_b4_01020304'); // ArrayBuffer { [Uint8Contents]: <00 ff 00 ff b4 01 02 03 04>, byteLength: 9 }
-   * const ZEROLENGTH = bx(''); // ArrayBuffer { [Uint8Contents]: <>, byteLength: 0 }
-   * ```
-   * 
-   * Examples of cases when it throws
-   * ```ts
-   * const WRONGHEX = bx('z ff 00 00'); // throws
-   * const MISSZERO = bx('f2-00_0_00'); // throws
-   * const SPACES = bx('          '); // throws
-   * ```
-   * 
-   * @param hex The string containing hex encoded data
-   * @returns An {@link ArrayBuffer} containing that data.
-   */
+/**
+ * A method that accepts a string containing hex encoded buffer.
+ * The string will be stripped of any non-hexadecimal symbols (eg: [^0-9-a-f]) and
+ * if the resulting string has even length then it's used as new Uint8Array(value).buffer
+ * or otherwise throws a TypeError.
+ *
+ * @example Examples of cases where it returns an ArrayBuffer
+ * ```ts
+ * const HEADER = bx('00 ff 00 ff'); // ArrayBuffer { [Uint8Contents]: <00 ff 00 ff>, byteLength: 4 }
+ * const SYNC1 = bx('00ff00ff_b4_01020304'); // ArrayBuffer { [Uint8Contents]: <00 ff 00 ff b4 01 02 03 04>, byteLength: 9 }
+ * const ZEROLENGTH = bx(''); // ArrayBuffer { [Uint8Contents]: <>, byteLength: 0 }
+ * ```
+ *
+ * @example Examples of cases when it throws
+ * ```ts
+ * const WRONGHEX = bx('z ff 00 00'); // throws
+ * const MISSZERO = bx('f2-00_0_00'); // throws
+ * const SPACES = bx('          '); // throws
+ * ```
+ *
+ * @param hex The string containing hex encoded data
+ * @returns An {@link ArrayBuffer} containing that data.
+ */
 export const bx = (hex: string): ArrayBuffer => {
   if (hex === '') {
     return new ArrayBuffer(0);
@@ -42,7 +42,7 @@ export const bx = (hex: string): ArrayBuffer => {
 
 /**
  * Exactly the same as {@link bx} just returns NodeJS Buffer instead of ArrayBuffer
- * 
+ *
  * @param hex The string containing hex encoded data
  * @returns A {@link Buffer} containing that data.
  */
